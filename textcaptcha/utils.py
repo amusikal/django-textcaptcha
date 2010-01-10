dc0d2ad023696b096eaf3d81d7e333f8d778086a
Mike Hurt <amusikal@yahoo.co.uk>
2010-01-10 13:39:39 +0000

    Removed test API key

--- textcaptcha/utils.py
+++ textcaptcha/utils.py
@@ -4,8 +4,7 @@ from hashlib import sha256, md5
 from lxml import objectify
 from django.conf import settings
 
-CAPTCHA_TEST_KEY = '2sbwf7wsbomcc8ccw008gc0gk'
-CAPTCHA_API_KEY = getattr(settings, 'CAPTCHA_API_KEY', CAPTCHA_TEST_KEY)
+CAPTCHA_API_KEY = getattr(settings, 'CAPTCHA_API_KEY')
 CAPTCHA_API_URL = 'http://textcaptcha.com/api/%s/'
 
 def retrieve_textcaptcha(api_key=CAPTCHA_API_KEY):
